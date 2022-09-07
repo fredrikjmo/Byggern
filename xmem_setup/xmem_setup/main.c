96ee9fb7364b3107e3ccc632f8bfe71776ec21f7
Akilan Jeyamenan <akilanj@ntnu.no>
2022-09-07 10:16:56 +0200

    add files for lab day 2

---
+++ xmem_setup/xmem_setup/main.c
@@ -0,0 +1,18 @@
+/*
+ * xmem_setup.c
+ *
+ * Created: 07.09.2022 09:02:38
+ * Author : akilanj
+ */ 
+
+#include <avr/io.h>
+#include "xmem.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    { 
+		
+    }
+}
