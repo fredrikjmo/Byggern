2f51ff547ecf559ce537c2a63190ff941604347a
Fredrik Johan Stokke Mo <fjmo@ntnu.no>
2022-10-26 12:10:48 +0200

    Added ADC init and read function for node 2

---
+++ Node 2/Node 2/Servo_and _IR/ADC.h
@@ -0,0 +1,19 @@
+/*
+ * ADC.h
+ *
+ * Created: 26.10.2022 11:30:48
+ *  Author: fjmo
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+void ADC_init ( void );
+
+uint32_t ADC_read ( void );
+
+
+
+
+#endif /* ADC_H_ */
